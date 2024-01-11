03857c31abb4e641a39497e2c4a322faca386eef
alexlofton <147463152+alexlofton@users.noreply.github.com>
2024-01-11 10:39:35 -0700

    updating routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,13 @@
-/// use express router to create get and post routes at /api/notes
+/// use express router to create get and post routes at localhost:3001/api
+const router = require("express").Router();
+const fs = require("fs");
+let db = require("../db/db.json");
+
+router.get("/notes", (req, res) => {
+    res.json(db)
+})
+
+
+
+
+module.exports = router
